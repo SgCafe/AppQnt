076bf55998cbfd48b391f39259c38adf9e8f61e3
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2023-02-18 18:27:12 -0300

    Adicionar arquivos de projeto.

---
+++ AppQnt/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQnt
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantidade = 1;
+        string pipo = "Vai botar na pipoquinha quantas vezes?";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            quantidade++;
+            lblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void Diminuir(object sender, EventArgs e)
+        {
+            if(quantidade != 0)
+            {
+                quantidade--;
+                lblQuantidade.Text = quantidade.ToString();
+            }
+            
+        }
+
+        private void Curioso(object sender, EventArgs e)
+        {
+            
+            lblAlternativo.Text = pipo.ToString();
+            
+        }
+    }
+}
